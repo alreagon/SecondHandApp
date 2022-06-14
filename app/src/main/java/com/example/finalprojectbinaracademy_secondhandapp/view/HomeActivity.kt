a22e1c1b35217c2bfa080aeb3852746173bab3e4
ALPHA <alpharajasa@gmail.com>
2022-06-14 19:56:44 +0700

    Gradle fix

---
+++ app/src/main/java/com/example/finalprojectbinaracademy_secondhandapp/view/HomeActivity.kt
@@ -0,0 +1,38 @@
+package com.example.finalprojectbinaracademy_secondhandapp.view
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.activity.compose.setContent
+import androidx.compose.runtime.Composable
+import com.example.finalprojectbinaracademy_secondhandapp.ui.theme.FinalProjectBinarAcademySecondHandAppTheme
+
+class HomeActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            FinalProjectBinarAcademySecondHandAppTheme {
+                
+            }
+        }
+    }
+}
+
+@Composable
+fun BottomNav() {
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
