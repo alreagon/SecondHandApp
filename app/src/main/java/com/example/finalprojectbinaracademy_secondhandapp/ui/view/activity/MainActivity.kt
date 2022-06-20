0a10f523a7375673b90cc3a57140ad12740ca9b6
Achsani Taqwim <achsanit87@gmail.com>
2022-06-20 08:38:37 +0800

    validating input regis, ui profil

--- app/src/main/java/com/example/finalprojectbinaracademy_secondhandapp/ui/view/activity/MainActivity.kt
+++ app/src/main/java/com/example/finalprojectbinaracademy_secondhandapp/ui/view/activity/MainActivity.kt
@@ -2,8 +2,11 @@ package com.example.finalprojectbinaracademy_secondhandapp.ui.view.activity
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.findNavController
+import androidx.navigation.ui.setupWithNavController
 import com.example.finalprojectbinaracademy_secondhandapp.R
 import com.example.finalprojectbinaracademy_secondhandapp.databinding.ActivityMainBinding
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
--- app/src/main/java/com/example/finalprojectbinaracademy_secondhandapp/ui/view/fragment/RegisterFragment.kt
+++ app/src/main/java/com/example/finalprojectbinaracademy_secondhandapp/ui/view/fragment/RegisterFragment.kt
@@ -1,6 +1,8 @@
 package com.example.finalprojectbinaracademy_secondhandapp.ui.view.fragment
 
 import android.os.Bundle
+import android.text.TextUtils
+import android.util.Patterns
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
@@ -11,6 +13,7 @@ import com.example.finalprojectbinaracademy_secondhandapp.R
 import com.example.finalprojectbinaracademy_secondhandapp.data.remote.model.RegisterRequest
 import com.example.finalprojectbinaracademy_secondhandapp.databinding.FragmentRegisterBinding
 import com.example.finalprojectbinaracademy_secondhandapp.ui.viewmodel.RegisterViewModel
+import com.example.finalprojectbinaracademy_secondhandapp.utils.PasswordUtils
 import org.koin.androidx.viewmodel.ext.android.viewModel
 
 class RegisterFragment : Fragment() {
@@ -39,17 +42,68 @@ class RegisterFragment : Fragment() {
     private fun registerUser() {
         val name = binding.etName.text.toString()
         val email = binding.etEmail.text.toString()
-        val phoneNumber = binding.etPhone.text.toString().toLong()
+        val phoneNumber = binding.etPhone.text.toString()
         val address = binding.etAddress.text.toString()
         val pass = binding.etPassword.text.toString()
 
-        val request = RegisterRequest(address,email,name,null,pass,phoneNumber)
+        if (inputCheck(name,email,phoneNumber,address,pass)) {
+            val request = RegisterRequest(address,email,name,null,pass,phoneNumber.toLong())
 
-        registerViewModel.userRegister(request)
+            registerViewModel.userRegister(request)
 
-        registerViewModel.userRegis.observe(requireActivity(), Observer {
-            Toast.makeText(requireContext(),"${it.email}, ${it.id}",Toast.LENGTH_SHORT).show()
-        })
+            registerViewModel.userRegis.observe(requireActivity(), Observer { user ->
+                user?.let {
+                    Toast.makeText(requireContext(),"${it.email}, ${it.id}, ${it.fullName}",Toast.LENGTH_SHORT).show()
+                }
+            })
+
+            Toast.makeText(requireContext(),"register....", Toast.LENGTH_SHORT).show()
+        } else {
+            validateErrorInput(name,email,phoneNumber,address,pass)
+        }
+    }
+
+    private fun validateErrorInput(name:String,email:String,phone:String,address:String,password:String) {
+        if (TextUtils.isEmpty(name)) {
+            binding.wrapName.error = "Masukkan nama anda"
+        } else {
+            binding.wrapName.error = null
+        }
+
+        if (!Patterns.EMAIL_ADDRESS.matcher(email).matches()) {
+            binding.wrapEmail.error = "Format email salah"
+        } else {
+            binding.wrapEmail.error = null
+        }
+
+        if (TextUtils.isEmpty(address)) {
+            binding.wrapAddress.error = "Masukkan alamat anda"
+        } else {
+            binding.wrapAddress.error = null
+        }
+
+        if (TextUtils.isEmpty(password)) {
+            binding.wrapPassword.error = "Masukkan password"
+        } else {
+            binding.wrapPassword.error = null
+        }
+
+        if (!PasswordUtils().validate(password)) {
+            binding.wrapPassword.error = "Harus mengandung upercase, lowercase, angka, dan min. 8 karakter"
+        } else {
+            binding.wrapPassword.error = null
+        }
+
+        if (TextUtils.isEmpty(phone)) {
+            binding.wrapPhone.error = "Masukkan nomor telepon anda"
+        } else {
+            binding.wrapPhone.error = null
+        }
+    }
+
+    private fun inputCheck(name: String, email:String, phone: String, address: String, pass: String): Boolean {
+        return !(TextUtils.isEmpty(name) || TextUtils.isEmpty(email) || TextUtils.isEmpty(pass) || !PasswordUtils().validate(pass) ||
+                TextUtils.isEmpty(phone) || TextUtils.isEmpty(address) || !Patterns.EMAIL_ADDRESS.matcher(email).matches() )
     }
 
 }
