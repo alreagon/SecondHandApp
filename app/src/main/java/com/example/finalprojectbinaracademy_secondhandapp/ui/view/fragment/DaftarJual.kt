b51bbfb612987d98d268b8f373486ce20248d2f9
Goldly26 <79614064+Goldly26@users.noreply.github.com>
2022-07-05 21:48:16 +0700

    ui Daftar jual nambah view pagger

--- app/src/main/java/com/example/finalprojectbinaracademy_secondhandapp/ui/view/fragment/DaftarJual.kt
+++ app/src/main/java/com/example/finalprojectbinaracademy_secondhandapp/ui/view/fragment/DaftarJual.kt
@@ -5,16 +5,28 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.viewpager.widget.ViewPager
 import com.example.finalprojectbinaracademy_secondhandapp.R
+import com.example.finalprojectbinaracademy_secondhandapp.databinding.FragmentDaftarJualBinding
+import com.example.finalprojectbinaracademy_secondhandapp.ui.adapter.MyPagerAdapter
+import kotlinx.android.synthetic.main.fragment_daftar_jual.*
 
 class DaftarJual : Fragment(R.layout.fragment_daftar_jual) {
+    private var _binding: FragmentDaftarJualBinding? = null
+    private val binding get() = _binding
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_daftar_jual, container, false)
+        _binding = FragmentDaftarJualBinding.inflate(inflater,container,false)
+        return binding?.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        vp_1.adapter = MyPagerAdapter(childFragmentManager)
     }
 
 }
